fcd4d731faf5510a175cd68e346a52f5be246c55
Hamit Seyrek <hamitseyrek@gmail.com>
2026-04-09 09:52:16 +0300

    [MOBILE-026] feat: add initial colorful scaffold layout with app bar, drawer, body and bottom navigation

---
+++ courses/mobile-programming-flutter/code/Flutter/flutter_started_kit/android/app/src/main/kotlin/com/example/flutter_started_kit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_started_kit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ courses/mobile-programming-flutter/code/Flutter/flutter_started_kit/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Flutter Kit'),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.orange,
+        ),
+      ),
+      body: const Center(
+        child: Text('Hello, World!'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.purple,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Ayarlar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+      ),
+    );
+  }
+}
