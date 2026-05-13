54d7e966b93158186dc62e2ee4f1af1e4f70593e
Hamit Seyrek <hamitseyrek@gmail.com>
2026-05-13 17:45:19 +0300

    [MOBILE-042] feat: AddNote ekranı eklendi, notlar sayfası güncellendi.

--- courses/mobile-programming-flutter/code/Flutter/full_note/lib/notes_page.dart
+++ courses/mobile-programming-flutter/code/Flutter/full_note/lib/notes_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:full_note/add_note.dart';
 import 'package:full_note/note_model.dart';
 
 class NotesPage extends StatefulWidget {
@@ -10,8 +11,14 @@ class NotesPage extends StatefulWidget {
 
 class _NotesPageState extends State<NotesPage> {
   final List<Note> _notes = [
-    Note(id: 1, title: 'ders',content: 'notlarım')
+    Note(id: 1, title: 'ders',content: 'notlarım'),
+    Note(id: 2, title: 'ders',content: 'notlarım'),
+    Note(id: 3, title: 'ders',content: 'notlarım'),
+    Note(id: 4, title: 'ders',content: 'notlarım'),
+    Note(id: 5, title: 'ders',content: 'notlarım'),
+    Note(id: 6, title: 'ders',content: 'notlarım')
     ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +37,28 @@ class _NotesPageState extends State<NotesPage> {
           return ListTile(
             title: Text(note.title),
             subtitle: Text(note.content),
+            trailing: Icon(Icons.arrow_forward),
           );
       }),
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          gotoAddNote(context);
+        },
+        child: const Icon(Icons.heart_broken_rounded),
+      ),
     );
+  }
+
+  void gotoAddNote(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const AddNote()),
+    ).then((newNote) {
+      if (newNote != null && newNote is Note) {
+        setState(() {
+          _notes.add(newNote);
+        });
+      }
+    });
   }
 }
